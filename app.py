29c528c05d4346978f86baad1b8f27c76e88c778
myname21-meet <myname21@meet.mit.edu>
2019-07-30 04:49:40 -0400

    fort

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     my_foods = ["puree batata", "lasagna", "cheese fries"]
+    my_least_foods = {"cat", "dog", "girafee"}
     return render_template(
     	"index.html",
-    	foods = my_foods)
+    	foods = my_foods,
+    	bad_foods = my_least_foods,
+    	opposite_day = True)
 
 if __name__ == '__main__':
    app.run(debug = True)
